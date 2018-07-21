40bad8ea929a48eb2752d1c004e252a17af3f193
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-07-21 14:23:16 -0400

    Connection with mysql dataSource (REST API)

--- WebAPI_Ex1.js
+++ WebAPI_Ex1.js
@@ -1,19 +1,30 @@
 var express = require('express');
 var app = express(); // need to use a consturctor to make the app work
 
-var jsonObj = require('./empSource');
-var employees = jsonObj.employees;
-console.log(employees);
+var mysql = require('mysql');
+var query = "";
+
+var cn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'happy',
+    database: 'employeedb'
+});  
 
 app.get('/', function(req, resp){
-    resp.send(employees);
+    query = "SELECT * FROM tblEmployees";
+    cn.query(query, function(err,data) {
+        resp.send(data);
+    });
 });
+
 
 app.get('/:id', function(req, resp){
     var id = req.params.id;
-   
-    var emp = employees.find(emp=>emp.id==id);
-    resp.send(emp);
+    query= "SELECT * FROM tblEmployees WHERE id=" + id;
+    cn.query(query, function(err,data) {
+    resp.send(data);
+});
 });
 
 app.listen(9000, function(){
