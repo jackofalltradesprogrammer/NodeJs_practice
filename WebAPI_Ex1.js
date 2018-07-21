7a47213ae5d7ce472c09e0672b807e7411b5abb0
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-07-21 13:38:48 -0400

    Importing data - find the exact object with find method

--- WebAPI_Ex1.js
+++ WebAPI_Ex1.js
@@ -1,26 +1,19 @@
 var express = require('express');
 var app = express(); // need to use a consturctor to make the app work
 
-var names = ['Suhil', 'Pavan', 'Ricky', 'Ujwal ', 'Happy'];
+var jsonObj = require('./empSource');
+var employees = jsonObj.employees;
+console.log(employees);
 
 app.get('/', function(req, resp){
-    resp.send(names);
+    resp.send(employees);
 });
 
-app.get('/:name', function(req, resp){
-    var name = req.params.name;
-    var flag=false;
-    for (let i = 0; i < names.length; i++) {
-        if (names[i] == name) {
-            flag=true;
-            break
-        }
-        
-    }
-    var msg='';
-    if (flag) msg=name+=' got selected';
-    else msg='namge got rejected';
-    resp.send(msg);
+app.get('/:id', function(req, resp){
+    var id = req.params.id;
+   
+    var emp = employees.find(emp=>emp.id==id);
+    resp.send(emp);
 });
 
 app.listen(9000, function(){
---
+++ empSource.js
@@ -0,0 +1,6 @@
+module.exports.employees=[
+    {id:101, ename:'Kiran', job:'Trainer', salary:5000},
+    {id:102, ename:'Happy', job:'Developer', salary:5000},
+    {id:103, ename:'David', job:'Student', salary:5000},
+    {id:104, ename:'Kratos', job:'Trainer', salary:5000}
+];
